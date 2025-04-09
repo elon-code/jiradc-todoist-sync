dfd320ecd40f5f8359a28e4934cbb5698fe82bc4
Elon Goliger <elon@slac.stanford.edu>
2025-04-09 11:44:09 -0700

    fix bat script

--- run_jira_sync.bat
+++ run_jira_sync.bat
@@ -1,5 +1,7 @@
 @echo off
 cd %~dp0
+git pull origin main
 git checkout main
+python -m pip install -r requirements.txt
 python main.py > run_jira_sync.log 2>&1
 echo Sync completed. Check run_jira_sync.log for details.
